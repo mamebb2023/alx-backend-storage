40530504a082d10e3be8e74fb842f9cbbf94be2c
Mohammednur <mamebb2023@gmail.com>
2023-12-20 19:26:54 +0300

    Exercise 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """ Redis With Python """
-import radis
+import redis
 from typing import Union
-
+from uuid import uuid4
 
 class Cache:
     def __init__(self) -> None:
         """ Init Radis """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Stores a data with a random key """
         random_key = str(uuid4())
-        self._radis.set(random_key, data)
+        self._redis.set(random_key, data)
         return random_key
